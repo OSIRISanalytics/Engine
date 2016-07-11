06f88077c95853a55f794e2b1a32bae54d7f2011
Tom Bishop <Tom@Toms-Air.gateway>
2016-07-11 19:33:14 +1000

    Update pinnacle R script

--- pinnacle.py
+++ pinnacle.py
@@ -32,28 +32,23 @@ function(uid, pword, sport) {
 	data = Odds %>%
 	        filter(LiveStatus != 1, PeriodNumber == 0) %>%
 	        select(match(keepers,names(Odds))) %>% 
-	        gather("Market_Value","Odds",
-	               spreads.home,spreads.away,
-	               moneyline.away,moneyline.home,moneyline.draw) %>%
-	        mutate(Market_Type = ifelse(grepl("moneyline",Market_Value), "moneyline",
-	                                    ifelse(grepl("home",Market_Value),"line.home","line.away")),
-	               Market_Value = ifelse(grepl("spreads",Market_Value), spreads.hdp, Market_Value),
+	        gather("Key","Odds",spreads.home,spreads.away,moneyline.away,moneyline.home,moneyline.draw) %>%
+	        mutate(Market_Type = ifelse(grepl("moneyline",Key), "moneyline",
+	                                    ifelse(grepl("home",Key),"line.home","line.away")),
+	               Market_Value = ifelse(grepl("spreads",Key), spreads.hdp,
+	                                     ifelse(Key=="moneyline.home",1, ifelse(Key=="moneyline.away",-1,0))),
 	               Market_Size = ifelse(Market_Type == "moneyline", maxMoneyline,maxSpread),
-	               Market_Odds = convertAmerican(Odds)) %>%
-	        select(-maxSpread, -maxMoneyline,-Odds, -spreads.hdp, -PeriodNumber) 
-	        
-	#remove any row with na values
+	               Market_Odds = convertAmerican(Odds),
+	               StartTime = format(as.POSIXct(strptime(StartTime,"%Y-%m-%dT%H:%M:%SZ", tz = "GMT")),tz = "Australia/Melbourne", usetz = TRUE)) %>%
+	        select(-maxSpread, -maxMoneyline,-Odds, -spreads.hdp, -PeriodNumber, -Key) %>%
+	        select(StartTime:AwayTeamName,Market_Type,Market_Value,Market_Odds,Market_Size) %>%
+	        rename(start_time=StartTime,sport_id=SportID,league_id=LeagueID,league_name = LeagueName,
+	               event_id=EventID,line_id=lineId,home_team=HomeTeamName,away_team=AwayTeamName)
+
+	names(data) = tolower(names(data))
 	data = data[complete.cases(data),]
+	data = cbind("time" = rep(Sys.time(), nrow(data)), data)
 
-	data$Market_Value =  revalue(data$Market_Value,
-	                      c("moneyline.home" = "1",
-	                      "moneyline.away" = "0",
-	                      "moneyline.draw" = "-1"))
-
-	data = data[,c(1:8,10,9,12,11)]
-
-	names(data) = c("start_time","sport_id", "league_id", "league_name", "event_id","line_id","home_team","away_team","market_type","market_value","market_odds","market_size")
-	  
 	return(data)
 
 }
