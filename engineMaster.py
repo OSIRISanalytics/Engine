c720eaa46c9c5b74e6b2aaef302e9216eeffebf4
Tom Bishop <tomwbish@gmail.com>
2016-08-02 20:15:50 +1000

    Random commit

--- engineMaster.py
+++ engineMaster.py
@@ -9,9 +9,8 @@ import datetime
 local = pytz.timezone ("Australia/Melbourne")
 time =  datetime.datetime.now().replace(tzinfo = local).strftime("%Y-%m-%d %H:%M:%S")
 
-# # Loop through and Log odds into the database
+# # # Loop through and Log odds into the database
 while time < "2016-07-29 20:00:00":
 	time =  datetime.datetime.now().replace(tzinfo = local).strftime("%Y-%m-%d %H:%M:%S")
 	utilities.getOdds("Soccer","Betfair",time,"Tom", Archive = True)
 	utilities.getOdds("Soccer","Pinnacle",time,"Tom", Archive = True)
-
